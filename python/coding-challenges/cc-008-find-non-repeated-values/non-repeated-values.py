b4634b4146425263b4820555ed1a1d3ad279ca48
Jokerinya <sakacibrahim@gmail.com>
2020-10-19 11:46:37 +0300

    non-repeated-values challenge done

---
+++ python/coding-challenges/cc-008-find-non-repeated-values/non-repeated-values.py
@@ -0,0 +1,9 @@
+products = ["Pride and Prejudice", "To Kill a Mockingbird", "The Great Gatsby",\
+"One Hundred Years of Solitude", "Pride and Prejudice", "In Cold Blood", "Wide Sargasso Sea",\
+"One Hundred Years of Solitude", "Brave New World",  "The Great Gatsby", "Brave New World",\
+"I Capture The Castle", "Brave New World", "The Great Gatsby", "The Great Gatsby",\
+"One Hundred Years of Solitude", "Pride and Prejudice"]
+
+
+for i in set(products):
+    if products.count(i) == 1: print(i)
