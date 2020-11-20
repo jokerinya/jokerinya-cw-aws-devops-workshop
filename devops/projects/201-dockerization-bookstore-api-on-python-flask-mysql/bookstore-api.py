5217c9b458c92f5ba2e9be7cf8156bf7efea5ecd
Jokerinya <sakacibrahim@gmail.com>
2020-11-21 00:11:21 +0200

    files added

--- devops/projects/201-dockerization-bookstore-api-on-python-flask-mysql/bookstore-api.py
+++ devops/projects/201-dockerization-bookstore-api-on-python-flask-mysql/bookstore-api.py
@@ -7,7 +7,7 @@ app = Flask(__name__)
 
 # Configure sqlite database
 app.config['MYSQL_DATABASE_HOST'] = 'database'
-app.config['MYSQL_DATABASE_USER'] = 'clarusway'
+app.config['MYSQL_DATABASE_USER'] = 'jokerinya'
 app.config['MYSQL_DATABASE_PASSWORD'] = 'Clarusway_1'
 app.config['MYSQL_DATABASE_DB'] = 'bookstore_db'
 app.config['MYSQL_DATABASE_PORT'] = 3306
